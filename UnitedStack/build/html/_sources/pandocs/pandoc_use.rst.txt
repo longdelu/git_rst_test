394e6e17bbd77e244f43261f906862073721def8
longdelu <861086182@qq.com>
2020-03-17 16:00:57 +0800

    官方的搜索

---
+++ UnitedStack/build/html/_sources/pandocs/pandoc_use.rst.txt
@@ -0,0 +1,20 @@
+pandoc使用
+==========
+
+word转rst
+---------
+
+例如一个word的文档为 ``test.docx``, 则该文档转换成rst输入下面該命令即可::
+
+  >> pandoc -s test.docx -o test.rst --extract-media=test_docx_pic
+
+
+.. attention::
+     其中，-s表示源文件；-o表示输出指定格式的目标文件，--extract-media=test_docx_pic 表示word文档的图片输出到指定的文件夹，这里示例为test_docx_pic。
+
+
+如果文件过长，输出过长时间未有发应，请重式或者把word文档按章节细拆分后再转换。转换完成后，请把转换生成的rst文件拷贝到指定的项目中，按照本文介绍的简单生成html文件的方法生成一个html文件看看效果，根据效果，细调表格，图片，引用，超链接，标题，代码块的效果。
+	 
+
+
+
--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,7 @@
 import os
 import sys
 
-#sys.path.append(os.path.abspath('_ext'))
+sys.path.append(os.path.abspath('_ext'))
 
 extensions = ['sphinx.ext.imgmath', 
               'sphinx.ext.todo', 
@@ -110,15 +110,15 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # print html_theme_options
-html_theme = 'sphinx_rtd_theme'
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = 'sphinx_rtd_theme'
+# import sphinx_rtd_theme
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme_options = {
 #     'logo_only': True,
 # }
-# html_theme = 'sphinx_ustack_theme'
-# import sphinx_ustack_theme
-# html_theme_path = [sphinx_ustack_theme.get_html_theme_path()]
+html_theme = 'sphinx_ustack_theme'
+import sphinx_ustack_theme
+html_theme_path = [sphinx_ustack_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
